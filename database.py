774ddd154280acc47f597557460a87b3a648c877
sudharsanK7 <94968082+sudharsanK7@users.noreply.github.com>
2023-12-07 12:33:09 +0000

    tweaked the outgoing submission route a bit to make it worth smooth

--- database.py
+++ database.py
@@ -30,20 +30,25 @@ def load_job_from_db(id):
       return row[0]._asdict()
 
 def add_application_to_db(job_id, data):
-    with engine.connect() as conn:
-      query = text('INSERT into applications (job_id, full_name, email, linkedin_url, '
-         'education, work_experience, resume_url) VALUES '
-         '(:job_id, :full_name, :email, :linkedin_url, :education, '
-         ':work_experience, :resume_url)')
+  try:
+      with engine.connect() as conn:
+          query = text('INSERT into applications (job_id, full_name, email, linkedin_url, '
+                         'education, work_experience, resume_url) VALUES '
+                         '(:job_id, :full_name, :email, :linkedin_url, :education, '
+                         ':work_experience, :resume_url)')
 
+          conn.execute(query, 
+                         {
+                             'job_id': job_id,  
+                             'full_name': data['full_name'],  
+                             'email': data['email'], 
+                             'linkedin_url': data['linkedin_url'],  
+                             'education': data['education'],  
+                             'work_experience': data['work_experience'], 
+                             'resume_url': data['resume_url']  
+                         })
 
-      conn.execute(query, 
-      {
-          'job_id': job_id,  
-          'full_name': data['full_name'],  
-          'email': data['email'], 
-          'linkedin_url': data['linkedin_url'],  
-          'education': data['education'],  
-          'work_experience': data['work_experience'], 
-          'resume_url': data['resume_url']  
-      })
+            # Add a commit statement
+          conn.commit()
+  except Exception as e:
+        print(f"Error adding application to the database: {e}")
